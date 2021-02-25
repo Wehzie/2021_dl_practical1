6c7dcbe77946af211b0cdf82e0d7ad99dd3d423e
Lonneke Langeveld <l.langeveld@gmail.com>
2021-02-25 12:08:58 +0100

    Updated code for all experiments

--- main.py
+++ main.py
@@ -74,20 +74,64 @@ def run_exp4(x_train, y_train, x_test, y_test):
     model.evaluate(x_test,  y_test, verbose=2)
 
 # Run experiment 5: 1st of experiments 1 to 4 with MAX pooling
-def run_exp5():
-    pass
+def run_exp5(x_train, y_train, x_test, y_test):
+    model = tf.keras.applications.MobileNet(
+    input_shape=(56,56,3),
+    include_top=False,
+    weights="imagenet",
+    pooling="max",
+    )
+
+    # Train and evaluate model
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+    model.evaluate(x_test,  y_test, verbose=1)
 
 # Run experiment 6: 2nd of experiments 1 to 4 with MAX pooling
-def run_exp6():
-    pass
+def run_exp6(x_train, y_train, x_test, y_test):
+    model = tf.keras.applications.DenseNet121(
+    input_shape=(56,56,3),
+    include_top=False,
+    weights="imagenet",
+    pooling="max",
+    )
+
+    # Train and evaluate model
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+    model.evaluate(x_test,  y_test, verbose=1)
 
 # Run experiment 7: 1st of experiments 1 to 6 with SDG optimization
-def run_exp7():
-    pass
+def run_exp7(x_train, y_train, x_test, y_test):
+    model = tf.keras.applications.DenseNet121(
+    input_shape=(56,56,3),
+    include_top=False,
+    weights="imagenet",
+    pooling="avg",
+    )
+
+    # Train and evaluate model
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer='SDG', loss=loss_fn, metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+    model.evaluate(x_test,  y_test, verbose=1)
 
 # Run experiment 8: 1st of experiments 1 to 6 with RMSprop optimizer
-def run_exp8():
-    pass
+def run_exp8(x_train, y_train, x_test, y_test):
+    model = tf.keras.applications.DenseNet121(
+    input_shape=(56,56,3),
+    include_top=False,
+    weights="imagenet",
+    pooling="avg",
+    )
+
+    # Train and evaluate model
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model.compile(optimizer='RMSprop', loss=loss_fn, metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+    model.evaluate(x_test,  y_test, verbose=1)
 
 def main():
     # Import data
@@ -103,6 +147,7 @@ def main():
     x_test = np.repeat(x_test[..., np.newaxis], 3, -1)
 
     # Run experiments
-    run_exp2(x_train, y_train, x_test, y_test)
+    run_exp5(x_train, y_train, x_test, y_test)
+    run_exp6(x_train, y_train, x_test, y_test)
 
 main()
